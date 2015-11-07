5b4947f551f61a3545e831b219b99abfb7de8cdc
abrinkmacmu <abrinkma@andrew.cmu.edu>
2015-11-07 17:38:00 -0500

    added ploting features

--- src/pskl_import_and_fold.py
+++ src/pskl_import_and_fold.py
@@ -12,7 +12,7 @@ file_loc = '/home/apark/Homework/ML_project1/'
 from sklearn.cross_validation import KFold
 from sklearn import svm
 from sklearn.feature_selection import SelectKBest
-
+import matplotlib.cm as cm
 # Import test data and labels
 import_data = sio.loadmat(file_loc + 'data/project1data.mat')
 import_labels = sio.loadmat(file_loc + 'data/project1data_labels.mat')
@@ -32,11 +32,41 @@ print("size of X_downsampled: ", X_downsampled.shape)
 
 # PCA
 from sklearn.decomposition import PCA
-pca = PCA(n_components = 10)
+pca = PCA(n_components = 2)
 pca.fit(X)
 print(pca.explained_variance_ratio_)
 X_pca = pca.transform(X)
 print( " X_pca share: ", X_pca.shape)
+
+
+# plot principal components
+index_1 = np.where(Y==1)[0]
+index_2 = np.where(Y==2)[0]
+index_3 = np.where(Y==3)[0]
+index_4 = np.where(Y==4)[0]
+index_5 = np.where(Y==5)[0]
+
+x = X_pca[index_1,:1]
+y = X_pca[index_1,1:2]
+print x
+print y
+plt.figure()
+plt.hold('on')
+
+plt.scatter(X_pca[index_1,:1],X_pca[index_1,1:2], color="r")
+plt.scatter(X_pca[index_2,:1],X_pca[index_2,1:2], color="b")
+plt.scatter(X_pca[index_3,:1],X_pca[index_3,1:2], color="g")
+plt.scatter(X_pca[index_4,:1],X_pca[index_4,1:2], color="c")
+plt.scatter(X_pca[index_5,:1],X_pca[index_5,1:2], color="m")
+
+plt.legend(("1","2","3","4","5"))
+plt.title("PCA of 2 largest components")
+
+plt.show()
+    
+#plt.plot()
+
+
 '''
 kf = KFold(len(Y),n_folds = 5)
 for train_index,test_index in kf:
